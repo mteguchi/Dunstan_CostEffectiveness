440c7d99a7dd1c8be85f3434e5e13f86dab21cdd
mteguchi <tomo.eguchi@noaa.gov>
2021-05-28 12:54:25 -0700

    Updating with new data

--- Dunstan_functions.R
+++ Dunstan_functions.R
@@ -2,22 +2,40 @@
 
 define.df_q50 <- function(x) { 
   y <- data.frame(t(x))
-  colnames(y) <- c("2014R_q50", "2017R_q50", 
-                   "North_q50", "South_q50", "West_q50")
+  if (ncol(y) == 5){
+    colnames(y) <- c("2014R_q50", "2017R_q50", 
+                     "North_q50", "South_q50", "West_q50")
+  } else if (ncol(y) == 6) {
+    colnames(y) <- c("2014R_q50", "2017R_q50", "2019R_q50",
+                     "North_q50", "South_q50", "West_q50")
+    
+  }
   y$DOSeason <- seq(1, nrow(y))
   return(y)}
 
 define.df_q2.5 <- function(x) { 
   y <- data.frame(t(x))
-  colnames(y) <- c("2014R_q2.5", "2017R_q2.5", 
-                   "North_q2.5", "South_q2.5", "West_q2.5")
+  if (ncol(y) == 5){
+    colnames(y) <- c("2014R_q2.5", "2017R_q2.5", 
+                     "North_q2.5", "South_q2.5", "West_q2.5")
+  } else if (ncol(y) == 6) {
+    colnames(y) <- c("2014R_q2.5", "2017R_q2.5", "2019R_q2.5",
+                     "North_q2.5", "South_q2.5", "West_q2.5")
+    
+  }
   y$DOSeason <- seq(1, nrow(y))
   return(y)}
 
 define.df_q97.5 <- function(x) { 
   y <- data.frame(t(x))
-  colnames(y) <- c("2014R_q97.5", "2017R_q97.5", 
-                   "North_q97.5", "South_q97.5", "West_q97.5")
+  if (ncol(y) == 5){
+    colnames(y) <- c("2014R_q97.5", "2017R_q97.5", 
+                     "North_q97.5", "South_q97.5", "West_q97.5")
+  } else if (ncol(y) == 6) {
+    colnames(y) <- c("2014R_q97.5", "2017R_q97.5", "2019R_q97.5",
+                     "North_q97.5", "South_q97.5", "West_q97.5")
+    
+  }
   y$DOSeason <- seq(1, nrow(y))
   return(y)}
 
@@ -45,7 +63,7 @@ extract.statistics <- function(jm){
   # Xs_q97.5.long <- melt(Xs_q97.5, value.name = "y_q97.5")
   
   seasons <- c(2014, 2015, 2016, 2017)
-  for (s in 1:4){
+  for (s in 1:length(seasons)){
     Xs_q50[[s]]$Season <- seasons[s]
     Xs_q2.5[[s]]$Season  <- seasons[s]
     Xs_q97.5[[s]]$Season  <- seasons[s]
@@ -86,6 +104,82 @@ extract.statistics <- function(jm){
   
   return(out = list(Xs_2014R = Xs_2014R,
                     Xs_2017R = Xs_2017R,
+                    Xs_North = Xs_North,
+                    Xs_South = Xs_South,
+                    Xs_West = Xs_West))
+  
+}
+
+extract.statistics.2021 <- function(jm){
+  ys_q50 <- apply(jm$q50$y, MARGIN = 1, FUN = define.df_q50)
+  ys_q2.5 <- apply(jm$q2.5$y, MARGIN = 1, FUN = define.df_q2.5)
+  ys_q97.5 <- apply(jm$q97.5$y, MARGIN = 1, FUN = define.df_q97.5)
+  
+  # ys_q2.5.long <- melt(ys_q2.5, value.name = "y_q2.5")
+  # ys_q50.long <- melt(ys_q50, value.name = "y_q50")
+  # ys_q97.5.long <- melt(ys_q97.5, value.name = "y_q97.5")
+  
+  Xs_q50 <- apply(jm$q50$X, MARGIN = 1, 
+                  FUN = define.df_q50)
+  
+  Xs_q2.5 <- apply(jm$q2.5$X, MARGIN = 1, FUN = define.df_q2.5)
+  
+  Xs_q97.5 <- apply(jm$q97.5$X, MARGIN = 1, FUN = define.df_q97.5)
+  
+  # Xs_q2.5.long <- melt(Xs_q2.5, value.name = "X_q2.5")
+  # 
+  # Xs_q50.long <- melt(Xs_q50, value.name = "X_q50")
+  # 
+  # Xs_q97.5.long <- melt(Xs_q97.5, value.name = "y_q97.5")
+  
+  seasons <- c(2014, 2015, 2016, 2017, 2018, 2019, 2020)
+  for (s in 1:length(seasons)){
+    Xs_q50[[s]]$Season <- seasons[s]
+    Xs_q2.5[[s]]$Season  <- seasons[s]
+    Xs_q97.5[[s]]$Season  <- seasons[s]
+    
+    ys_q50[[s]]$Season <- seasons[s]
+    ys_q2.5[[s]]$Season  <- seasons[s]
+    ys_q97.5[[s]]$Season  <- seasons[s]
+  }
+  
+  Xs_q2.5_df <- do.call(rbind, Xs_q2.5)
+  Xs_q50_df <- do.call(rbind, Xs_q50)
+  Xs_q97.5_df <- do.call(rbind, Xs_q97.5)
+  
+  Xs_2014R <- cbind(Xs_q2.5_df$`2014R_q2.5`, Xs_q50_df$`2014R_q50`,
+                    Xs_q97.5_df$`2014R_q97.5`, Xs_q2.5_df[, c("DOSeason", "Season")])
+  colnames(Xs_2014R) <- c("Xs_q2.5", "Xs_q50", "Xs_q97.5", "DOSeason", "Season")
+  Xs_2014R$Sector <- "2014R"
+  
+  Xs_2017R <- cbind(Xs_q2.5_df$`2017R_q2.5`, Xs_q50_df$`2017R_q50`,
+                    Xs_q97.5_df$`2017R_q97.5`, Xs_q2.5_df[, c("DOSeason", "Season")])
+  colnames(Xs_2017R) <- c("Xs_q2.5", "Xs_q50", "Xs_q97.5", "DOSeason", "Season")
+  Xs_2017R$Sector <- "2017R"
+  
+  Xs_2019R <- cbind(Xs_q2.5_df$`2019R_q2.5`, Xs_q50_df$`2019R_q50`,
+                    Xs_q97.5_df$`2019R_q97.5`, Xs_q2.5_df[, c("DOSeason", "Season")])
+  colnames(Xs_2019R) <- c("Xs_q2.5", "Xs_q50", "Xs_q97.5", "DOSeason", "Season")
+  Xs_2019R$Sector <- "2019R"
+
+  Xs_North <- cbind(Xs_q2.5_df$North_q2.5, Xs_q50_df$North_q50,
+                    Xs_q97.5_df$North_q97.5, Xs_q2.5_df[, c("DOSeason", "Season")])
+  colnames(Xs_North) <- c("Xs_q2.5", "Xs_q50", "Xs_q97.5", "DOSeason", "Season")
+  Xs_North$Sector <- "North"
+  
+  Xs_South <- cbind(Xs_q2.5_df$South_q2.5, Xs_q50_df$South_q50,
+                    Xs_q97.5_df$South_q97.5, Xs_q2.5_df[, c("DOSeason", "Season")])
+  colnames(Xs_South) <- c("Xs_q2.5", "Xs_q50", "Xs_q97.5", "DOSeason", "Season")
+  Xs_South$Sector <- "South"
+  
+  Xs_West <- cbind(Xs_q2.5_df$West_q2.5, Xs_q50_df$West_q50,
+                   Xs_q97.5_df$West_q97.5, Xs_q2.5_df[, c("DOSeason", "Season")])
+  colnames(Xs_West) <- c("Xs_q2.5", "Xs_q50", "Xs_q97.5", "DOSeason", "Season")
+  Xs_West$Sector <- "West"
+  
+  return(out = list(Xs_2014R = Xs_2014R,
+                    Xs_2017R = Xs_2017R,
+                    Xs_2019R = Xs_2019R,
                     Xs_North = Xs_North,
                     Xs_South = Xs_South,
                     Xs_West = Xs_West))
